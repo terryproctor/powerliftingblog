4b46ac0bd27b2ba61a02603d6f2c33a1db77d819
terryproctor <terryproctor2015@outlook.com>
2020-10-24 22:38:24 +0100

    pb object (constructor)

--- JS/pbs.js
+++ JS/pbs.js
@@ -1,13 +1,20 @@
 //bench squat and deadlift records as variables
+function PBs (weight = 81.5, bench, squat, deadlift) {
+    this.weight = weight;
+    this.bench = bench;
+    this.squat = squat;
+    this.deadlift = deadlift;
+}
 
+let latestPBs = new PBs (
+    weight=81.5, bench=89.8, squat=170.6, deadlift=197.3);
 
-let bp = 90;
-let sq = 170;
-let dl = 197;
+let elWeight = document.getElementById("weight");
+elWeight.textContent = latestPBs.weight; 
 
 let elBP = document.getElementById("benchPB");
-elBP.textContent = bp + 'kg';
+elBP.textContent = `${latestPBs.bench}kg`;
 let elSQ = document.getElementById("squatPB");
-elSQ.textContent = sq + 'kg';
+elSQ.textContent = `${latestPBs.squat}kg`;
 let elDL = document.getElementById("deadliftPB");
-elDL.textContent = dl + 'kg';
+elDL.textContent = `${latestPB.s}kg`;
