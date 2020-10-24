3fba5dba833872014fbd43b6b849fe078e736623
terryproctor <terryproctor2015@outlook.com>
2020-10-24 21:54:27 +0100

    js pbs

---
+++ JS/pbs.js
@@ -0,0 +1,13 @@
+//bench squat and deadlift records as variables
+
+
+let bp = 90;
+let sq = 170;
+let dl = 197;
+
+let elBP = document.getElementById("benchPB");
+elBP.textContent = bp + 'kg';
+let elSQ = document.getElementById("squatPB");
+elSQ.textContent = sq + 'kg';
+let elDL = document.getElementById("deadliftPB");
+elDL.textContent = dl + 'kg';
